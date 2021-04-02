695820dd03bdd6fe679f73ef1ed6d246b15c2691
samipourquoi <sami.guiderdoni@gmail.com>
2021-04-03 00:30:26 +0200

    othello: make button to show header on small screen sizes

--- packages/othello/src/components/new/header.tsx
+++ packages/othello/src/components/new/header.tsx
@@ -2,21 +2,36 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-export const NewHeader: React.FC = () => (
-  <header className="
-    bg-gray-100 h-screen text-gray-400 rounded-r-3xl text-xl text-black
-    flex flex-col relative
-    md:rounded-r-none
-  ">
-    <div className="overflow-y-auto p-7">
-      <Profile/>
-      <Navigation/>
-      <SearchBar/>
-      <TagList/>
+export const NewHeader: React.FC = () => {
+  const [isForcedVisible, setForcedVisible] = useState(false);
+
+  return (
+    <div className="flex h-screen">
+      <header className={`
+        ${isForcedVisible ? "flex" : "hidden"} bg-gray-50 h-screen text-gray-400 text-xl text-black
+        flex flex-col relative border
+        md:flex
+        md:rounded-r-none
+      `}>
+        <div className="overflow-y-auto p-7">
+          <Profile/>
+          <Navigation/>
+          <SearchBar/>
+          <TagList/>
+        </div>
+        <OptionsBar/>
+      </header>
+
+      <div className={`
+        material-icons text-gray-400 shadow ${isForcedVisible ? "" : "absolute"}
+        top-0 bg-gray-50 w-12 h-12 rounded-xl m-3 flex justify-center items-center
+        md:hidden
+        `} onClick={() => setForcedVisible(!isForcedVisible)}>
+        {isForcedVisible ? "navigate_before" : "navigate_next"}
+      </div>
     </div>
-    <OptionsBar/>
-  </header>
-);
+  );
+}
 
 const Profile: React.FC = () => (
   <div className="flex items-center">
@@ -66,7 +81,8 @@ const SearchBar: React.FC = () => {
     <div className="py-9 my-9 border-t border-b border-gray-300">
       <h2 className="mb-5 text-2xl">Search</h2>
       <input type="text" placeholder={placeholder} className="
-        w-full outline-none border-none p-3.5 rounded-xl text-base font-light text-gray-600
+        w-full outline-none border-none p-3.5 rounded-xl text-base font-light
+        text-gray-600 shadow
       "/>
     </div>
   );
@@ -112,8 +128,10 @@ const OptionsBar: React.FC = () => {
   const [darken, setDarken] = useState(false);
 
   return (
-    <div className="min-h-10 p-7 py-5 flex justify-start items-center shadow-2xl">
-      <div className="material-icons cursor-pointer hover:text-gray-500" onClick={() => {
+    <div className="min-h-10 p-7 py-5 flex justify-start items-center">
+      <div className="material-icons mr-5">settings</div>
+
+      <div className="material-icons cursor-pointer text-gray-400 hover:text-gray-500" onClick={() => {
         setDarken(!darken);
       }}>
         {darken ? "dark_mode" : "light_mode"}
