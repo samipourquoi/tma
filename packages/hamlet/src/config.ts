3db9bf09ca2524b7797df09550cff4d605bff278
samipourquoi <sami.guiderdoni@gmail.com>
2021-06-06 13:46:59 +0200

    hamlet: validate config with io-ts

--- packages/hamlet/src/config.ts
+++ packages/hamlet/src/config.ts
@@ -1,15 +1,29 @@
 import * as YAML from "yaml";
 import * as fs from "fs";
+import * as t from "io-ts";
+import { PathReporter } from "io-ts/PathReporter";
+import { pipe, identity } from "fp-ts/function";
+import { fold, isLeft } from "fp-ts/Either";
 
-export interface Config {
-  auth: {
-    clientID: string,
-    clientSecret: string,
-    callbackURL: string
-  }
-}
+const ConfigCodec = t.type({
+  auth: t.type({
+    clientID: t.string,
+    clientSecret: t.string,
+    callbackURL: t.string
+  })
+});
 
-export function loadConfig(): Config {
+type Config = t.TypeOf<typeof ConfigCodec>;
+
+export const loadConfig = (): Config => {
   const file = fs.readFileSync("../../config/config.yml", "utf-8");
-  return YAML.parse(file);
+  const parsed: unknown = YAML.parse(file);
+  const decoded = ConfigCodec.decode(parsed);
+
+  if (isLeft(decoded))
+    throw new Error(PathReporter.report(decoded).join("\n"))
+  else return decoded.right;
 }
+
+export const validate = (config: unknown): config is Config =>
+  ConfigCodec.is(config);
